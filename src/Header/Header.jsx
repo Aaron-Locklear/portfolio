ef301e9b7ce2aa1fa9338c40e1c638e0064e275d
Aaron Locklear <aaronbenjaminlocklear@gmail.com>
2023-10-12 16:41:00 -0600

    Add new logo to header

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,13 +1,17 @@
 import "./Header.css"
 import { NavLink } from 'react-router-dom';
 import Logo from "./Logo";
+import NameLogo from "../NameLogo/NameLogo";
 
 
 export default function Header(){
   const PAGE_NAMES = ["Home","About","Projects"];
 
   return(<nav>
-    <Logo/>
+    <NameLogo firstInitial="A"
+                firstName="Aaron"
+                lastInitial="L"
+                lastName="Locklear"/>
     <ul>
       {PAGE_NAMES.map((pageName) => {
         return( <li key={pageName}><NavLink  to={"/" + pageName} activeclassname="active">{pageName}</NavLink></li> )
---
+++ src/NameLogo/NameLogo.jsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+import "./NameLogo.css";
+import ResetableTypingEffect from "./ResetableTypingEffect.js";
+
+export default function NameLogo({ firstInitial, firstName, lastName, lastInitial }) {
+  const [isHovered, setIsHovered] = useState(false);
+  const [resetText, setResetText] = useState(false);
+
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+    setResetText(false);
+  };
+
+  const handleMouseExit = () => {
+    setIsHovered(false);
+    setResetText(true);
+  };
+
+  const typedFirstName = ResetableTypingEffect(firstName, 100, resetText);
+  const typedLastName = ResetableTypingEffect(lastName, 100, resetText);
+
+  const currentFirstNameText = isHovered ? typedFirstName : firstInitial;
+  const currentLastNameText = isHovered ? typedLastName : lastInitial;
+
+  return (
+    <p className="full-name"
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseExit}>
+      <span className="first-name">{currentFirstNameText}</span>
+      {isHovered ? "\u00A0" : ""}
+      <span className="last-name">{currentLastNameText}</span>
+      .
+    </p>
+  );
+}

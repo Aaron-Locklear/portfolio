c3df6179d9e6ec09f1840a4897d292a87535f4ca
Aaron Locklear <aaronbenjaminlocklear@gmail.com>
2023-10-18 11:08:26 -0600

    Remove node_modules and fix submodule issue

--- src/Projects/projectsList.js
+++ src/Projects/projectsList.js
@@ -3,14 +3,14 @@ export const projectList = [
     title: 'Portfolio',
     description:
       'This portfolio, built with React, emphasizes the use of React Router and modular component design. It showcases my web development skills and design aesthetics in a cohesive presentation.',
-    imageURL: '../assets/portfolio.png',
+    imageURL: 'src/assets/portfolio.png',
     livePreviewURL: '.',
     sourceCodeURL: 'https://github.com/Aaron-Locklear/portfolio',
   },
   {
     title: 'HTML FORM',
     description: 'A demonstration of html forms and JavaScript validation',
-    imageURL: '../assets/SignUpForm.png',
+    imageURL: 'src/assets/SignUpForm.png',
     livePreviewURL: 'https://aaron-locklear.github.io/signUpForm/',
     sourceCodeURL: 'https://github.com/Aaron-Locklear/signUpForm',
   },

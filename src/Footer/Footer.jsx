2c39cb7394e9c7bfe77ca9a57dba9f707421375d
Aaron Locklear <aaronbenjaminlocklear@gmail.com>
2023-10-18 11:12:46 -0600

    Experiment with image links

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -3,10 +3,10 @@ import './Footer.css';
 export default function Footer() {
   return (
     <footer>
-      <img src="src/assets/github.svg" alt="Github Logo" />
-      <img src="src/assets/instagram.svg" alt="Instagram Logo" />
-      <img src="src/assets/linkedin.svg" alt="LinkedIn Logo" />
-      <img src="src/assets/mail.svg" alt="Email Logo" />
+      <img src="../assets/github.svg" alt="Github Logo" />
+      <img src="../assets/instagram.svg" alt="Instagram Logo" />
+      <img src="../assets/linkedin.svg" alt="LinkedIn Logo" />
+      <img src="../assets/mail.svg" alt="Email Logo" />
     </footer>
   );
 }

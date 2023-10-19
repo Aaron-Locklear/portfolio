13636f33706ad63109be50fef5cd038b1b92ab14
Aaron Locklear <aaronbenjaminlocklear@gmail.com>
2023-10-18 20:48:20 -0600

    Add style to link

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer>
       <img src="https://raw.githubusercontent.com/Aaron-Locklear/image-collection/6bf303a02d8e47ab865958577af31ff04218c06a/assets/github.svg" alt="Github Logo" />
-      <a href="https://www.instagram.com/aaron.b.locklear/"><img src="https://raw.githubusercontent.com/Aaron-Locklear/image-collection/6bf303a02d8e47ab865958577af31ff04218c06a/assets/instagram.svg" alt="Instagram Logo" /></a>
+      <a className="footer-link" href="https://www.instagram.com/aaron.b.locklear/"><img src="https://raw.githubusercontent.com/Aaron-Locklear/image-collection/6bf303a02d8e47ab865958577af31ff04218c06a/assets/instagram.svg" alt="Instagram Logo" /></a>
       <img src="https://raw.githubusercontent.com/Aaron-Locklear/image-collection/6bf303a02d8e47ab865958577af31ff04218c06a/assets/linkedin.svg" alt="LinkedIn Logo" />
       <img src="https://raw.githubusercontent.com/Aaron-Locklear/image-collection/6bf303a02d8e47ab865958577af31ff04218c06a/assets/mail.svg" alt="Email Logo" />
     </footer>
